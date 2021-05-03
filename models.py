5d25867b07420bab903bca2f6bdba050d611dd59
Siequnu <11821606+Siequnu@users.noreply.github.com>
2021-05-03 09:06:25 +0800

    Fix collab check bug

--- models.py
+++ models.py
@@ -63,13 +63,16 @@ def create_new_firepad():
 
 # Returns true if user is admin, owner, or collab in a firepad
 def check_if_user_has_access_to_firepad(firepad_id, user_id):
-	if app.models.is_admin(
-		current_user.username) or Firepad.query.get(
-		firepad_id).owner_id == current_user.id:
-		return True
-	elif check_if_user_is_a_collaborator(firepad_id, user_id):
-		return True
-	else:
+	try:
+		if app.models.is_admin(
+			current_user.username) or Firepad.query.get(
+			firepad_id).owner_id == current_user.id:
+			return True
+		elif check_if_user_is_a_collaborator(firepad_id, user_id):
+			return True
+		else:
+			return False
+	except:
 		return False
 	
 
